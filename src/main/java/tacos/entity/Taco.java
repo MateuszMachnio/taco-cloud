8e5cda2d3394d56d301198e99d3f8f5d289785ee
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-10-10 15:06:29 +0200

    validating Taco class

--- src/main/java/tacos/entity/Taco.java
+++ src/main/java/tacos/entity/Taco.java
@@ -10,8 +10,10 @@ import lombok.Data;
 public class Taco {
 
 	@NotNull
-	@Size
+	@Size(min=5, message = "Name must be at least 5 characters long")
 	private String name;
+	
+	@Size(min = 1, message = "You must choose at least 1 ingredient")
 	private List<String> ingredients;
 	
 }
