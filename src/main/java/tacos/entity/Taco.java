618ec5b5d80ce8a7d4923f505597962d18919363
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-10-10 10:21:36 +0200

    checking the change of settings

--- src/main/java/tacos/entity/Taco.java
+++ src/main/java/tacos/entity/Taco.java
@@ -1,12 +1,15 @@
 package tacos.entity;
 
 import java.util.List;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
 @Data
 public class Taco {
 
+	@NotNull
+	@Size
 	private String name;
 	private List<String> ingredients;
 	
