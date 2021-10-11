a7068a0f00d27bcfb801e7cacd531b16bc822b7d
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-10-11 10:59:53 +0200

    displaying Ingredients in redisplayed form

--- src/main/java/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -49,8 +49,27 @@ public class DesignTacoController {
 	}
 	
 	@PostMapping
-	public String processDesign(@Valid Taco taco, Errors errors) {
+	public String processDesign(@Valid Taco taco, Errors errors, Model model) {
 		if (errors.hasErrors()) {
+			
+			List<Ingredient> ingredients = Arrays.asList(
+					new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+					new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+					 new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+					 new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+					 new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+					 new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+					 new Ingredient("CHED", "Cheddar", Type.CHEESE),
+					 new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+					 new Ingredient("SLSA", "Salsa", Type.SAUCE),
+					 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+					);
+			
+			Type[] types = Ingredient.Type.values();
+			for (Type type : types) {
+				model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+			}
+			
 			return "design";
 		}
 		// to do
