64ac0e8202e5e49a4d45552300cbf8fbc09e2a86
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-10-19 15:33:02 +0200

    correct findByZip method name

--- src/main/java/tacos/repository/OrderRepository.java
+++ src/main/java/tacos/repository/OrderRepository.java
@@ -8,6 +8,6 @@ import tacos.entity.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 	
-	List<Order> findByDeliveryZip(String deliveryZip);
+	List<Order> findByZip(String deliveryZip);
 	
 }
