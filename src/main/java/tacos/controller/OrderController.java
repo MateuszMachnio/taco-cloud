6e8d23d4165ca0e1ff821e259249a4451c3a9487
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-10-22 14:55:14 +0200

    adding user to processing order

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -2,6 +2,7 @@ package tacos.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import tacos.entity.Order;
+import tacos.entity.User;
 import tacos.repository.OrderRepository;
 
 @Controller
@@ -30,11 +32,12 @@ public class OrderController {
 	}
 	
 	@PostMapping
-	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+	public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
 		if (errors.hasErrors()) {
 			return "orderForm";
 		}
 		
+		order.setUser(user);
 		orderRepo.save(order);
 		sessionStatus.setComplete();
 		
