922eb4ec4658b0aad0aa6ec8f10c3b034e86c06c
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-10-28 22:21:09 +0200

    created my first RestController

--- src/main/java/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -2,18 +2,26 @@ package tacos.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import tacos.entity.Ingredient;
@@ -23,17 +31,17 @@ import tacos.repository.TacoRepository;
 import tacos.entity.Order;
 import tacos.entity.Taco;
 
-@Controller
-@RequestMapping("/design")
-@SessionAttributes("order")
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
 public class DesignTacoController {
 	
-	private final IngredientRepository ingredientRepo;
 	private final TacoRepository tacoRepo;
 	
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
-		this.ingredientRepo = ingredientRepo;
+	EntityLinks entityLinks;
+	
+	public DesignTacoController(TacoRepository tacoRepo) {
 		this.tacoRepo = tacoRepo;
 	}
 	
@@ -45,6 +53,22 @@ public class DesignTacoController {
 	@ModelAttribute(name = "taco")
 	public Taco taco() {
 		return new Taco();
+	}
+	
+	@GetMapping("/recent")
+	public Iterable<Taco> recentTacos() {
+		PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+		return tacoRepo.findAll(page).getContent();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+		Optional<Taco> optTaco = tacoRepo.findById(id);
+		if (optTaco.isPresent()) {
+			return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		
 	}
 
 	@GetMapping
