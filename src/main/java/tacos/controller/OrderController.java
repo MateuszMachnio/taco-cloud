d7d0bcf40f7da892f3f36d0c65d65a01bdfa90ad
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-10-29 14:35:40 +0200

    deleteOrder method

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -2,18 +2,22 @@ package tacos.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -92,6 +96,16 @@ public class OrderController {
 		}
 		
 		return orderRepo.save(order);
+	}
+	
+	@DeleteMapping("/{orderId}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteOrder(@PathVariable Long orderId) {
+		try {
+			orderRepo.deleteById(orderId);
+		} catch (EmptyResultDataAccessException e) {
+			
+		}
 	}
 	
 }
