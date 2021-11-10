9b9b9cb90b3f30d241c422b4a1a2b4875c6109af
MateuszMachnio <mateusz.machnio.9@gmail.com>
2021-11-10 09:24:32 +0100

    added tacoProcessor Bean

--- src/main/java/tacos/webConfiguration/WebConfig.java
+++ src/main/java/tacos/webConfiguration/WebConfig.java
@@ -1,8 +1,15 @@
 package tacos.webConfiguration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import tacos.entity.Taco;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
@@ -11,6 +18,19 @@ public class WebConfig implements WebMvcConfigurer{
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/login");
+	}
+	
+	@Bean
+	public RepresentationModelProcessor<PagedModel<EntityModel<Taco>>> tacoProcessor (EntityLinks links) {
+		return new RepresentationModelProcessor<PagedModel<EntityModel<Taco>>>() {
+			@Override
+			public PagedModel<EntityModel<Taco>> process (PagedModel<EntityModel<Taco>> resource) {
+				resource.add(links.linkFor(Taco.class)
+								  .slash("recent")
+								  .withRel("recents"));
+				return resource;
+			}
+		};
 	}
 
 }
